603c32d77ca038e15d9fb9c122f7e85475ca7b54
Mario-Snow <salazarmariot@gmail.com>
2017-12-26 03:00:23 -0500

    conexion con la base de datos mongoDB

--- app/src/main/java/com/mario_salazar/hospital/LoginModel.java
+++ app/src/main/java/com/mario_salazar/hospital/LoginModel.java
@@ -1,5 +1,16 @@
 package com.mario_salazar.hospital;
 
+import android.os.StrictMode;
+
+import org.json.JSONArray;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import java.net.URLEncoder;
+import org.json.JSONArray;
+import org.json.JSONObject;
 /**
  * Created by Mario-Snow on 26/12/2017.
  */
@@ -14,10 +25,38 @@ public class LoginModel implements Login.Model {
 
     @Override
     public void login(String user, String pass) {
-        if(user.equals("admin") && pass.equals("12345")){
+        /*if(user.equals("admin") && pass.equals("12345")){
             presenter.showLogin("ok");
         }else{
             presenter.showLogin("nook");
-        }
+        }*/
+
+            try {
+                HttpClient httpclient = new DefaultHttpClient();
+
+                String q = "https://api.mongolab.com/api/1/databases/hospital/collections/usuario?q=" +
+                        URLEncoder.encode("{\"id\":'" + user + "',\"contra\":'" + pass + "' }") +
+                        "&fo=true&apiKey=edbsdtUKazd_h2nqfQRtSvhPpUuMNxCv";
+                System.out.println(q);
+                HttpGet get = new HttpGet(q);
+                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                StrictMode.setThreadPolicy(policy);
+                HttpResponse httpResponse = httpclient.execute(get);
+                String respStr = EntityUtils.toString(httpResponse.getEntity());
+
+                if (!respStr.equals(" null ")) {
+                    presenter.showLogin("ok");
+
+                } else {
+                    presenter.showLogin("nook");
+                }
+
+                //startActivity(new Intent(getApplicationContext(), Asistencia.class));
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+
     }
+
+
 }
